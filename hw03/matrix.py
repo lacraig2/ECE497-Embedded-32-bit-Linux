3dc23b5e79132a9e40dfdbf51fb91c60801a8cb0
Luke Craig <craigla@rose-hulman.edu>
2017-09-26 15:48:00 -0400

    adding some setup stuff in addition to some of my hw4 work

--- hw03/matrix.py
+++ hw03/matrix.py
@@ -2,19 +2,36 @@
 # Read a TMP101 sensor
 
 import smbus
-import time
+from time import sleep
+import Adafruit_BBIO.GPIO as GPIO
+
+
 bus = smbus.SMBus(1)
 address = 0x48
 address2 = 0x4A
-
-bus.write_byte_data(address,2,23)
+bus.write_byte_data(address,1,0x6)
+bus.write_byte_data(address,2,22)
 bus.write_byte_data(address,3,27)
-bus.write_byte_data(address2,2,23)
+bus.write_byte_data(address2,1,0x6)
+bus.write_byte_data(address2,2,22)
 bus.write_byte_data(address2,3,27)
-GPIO.setup("GPIO1_3", GPIo.
+GPIO.setup("GP0_3", GPIO.IN)
+GPIO.setup("GP0_4", GPIO.IN)
 
+def print_temp(channel):
+	print("ALERT")
+	bus.write_byte_data(address,1,0x6)
+	bus.write_byte_data(address2,1,0x6)
+	# temp = bus.read_byte_data(address, 0)
+	# temp2 = bus.read_byte_data(address2, 0)
+	# print(str(temp)+ " "+str(temp2))
+
+GPIO.add_event_detect("GP0_3", GPIO.BOTH, callback=print_temp)
+GPIO.add_event_detect("GP0_4", GPIO.BOTH, callback=print_temp)
 while True:
-    temp = bus.read_byte_data(address, 0)
-    temp2 = bus.read_byte_data(address2,0)
-    print(str(temp)+" "+str(temp2),  end="\r")
-    time.sleep(0.25)
+	temp = bus.read_byte_data(address, 0)
+	temp2 = bus.read_byte_data(address2, 0)
+	print(str(temp)+ " "+str(temp2))
+	print(str(hex(bus.read_byte_data(address,1)))+" "+str(hex(bus.read_byte_data(address2,1))))
+	sleep(0.5)
+GPIO.cleanup()
---
+++ setup/my_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+BONE=${1:-192.168.7.2}
+USER=debian
+./setDNS.sh $BONE
+./ipMasquerade.sh enp1s0f0
+./ipMasquerade.sh wlp2s0
+
+
